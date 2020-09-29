51e1ecf6684d8bf1a4880c6a74fe08ce046a6330
Nam Tran <namtran211101@gmail.com>
2020-09-29 15:50:44 +1000

    Updates to options pages

--- eBay-gumtree-data/background.js
+++ eBay-gumtree-data/background.js
@@ -1,6 +1,14 @@
 // background.js
-
+var user_name = "";
 // Called when the user clicks on the browser action.
+chrome.contextMenus.create({
+  title: "Your title here",
+  contexts: ["browser_action"],
+  onclick: function() {
+      chrome.tabs.create({ url: chrome.runtime.getURL("options.html") });
+      console.log("click");
+  }
+});
 chrome.browserAction.onClicked.addListener(function(tab) {
     // Send a message to the active tab
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -12,27 +20,30 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "open_new_tab" ) {
       copyToClipboard(request.data);
+      var opt = {
+        type: "basic",
+        title: "Primary Title",
+        message: "Primary message to display",
+        iconUrl: "img/icon.png"
+      }
+      chrome.notifications.create("", opt, callback);
     }
   }
 );
 function copyToClipboard(str) {
   // Create a dummy input to copy the string array inside it
-  alert(str);
   var dummy = document.createElement("input");
   // Add it to the document
   document.body.appendChild(dummy);
   // Set its ID
   dummy.setAttribute("id", "dummy_id");
-
-  // Output the array into it
   document.getElementById("dummy_id").value=str;
-
-  // Select it
+  // Select, copy and remove after
   dummy.select();
-
-  // Copy its contents
   document.execCommand("copy");
-
-  // Remove it as its not needed anymore
   document.body.removeChild(dummy);
+}
+function callback() {
+  console.log("Notification succesfull");
+  //notification confirmed
 }
--- eBay-gumtree-data/content.js
+++ eBay-gumtree-data/content.js
@@ -1,4 +1,3 @@
-
 // content.js
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
@@ -6,15 +5,17 @@ chrome.runtime.onMessage.addListener(
         var url = window.location.href;
         var p_title = $('title').text().split("|");
         
-        var ad_name = p_title[0];
-        var ad_id = p_title[p_title.length-1];
-        var price = $('.j-original-price').text(); 
-        var stock_id = $('#ad_description_details_content').text().split("#")[1].substring(0,5);
-        var date = $('#ad-attributes dd:first').text();
-        console.log(url);
-        console.log(price);
-        console.log(date);
-        var output = price + "\t" + ad_id;
+        name = "Nam";
+        let val = [];
+        val.push($('#ad_description_details_content').text().split("#")[1].substring(0,5));
+        val.push(p_title[0]);
+        val.push(p_title[p_title.length-1]);
+        val.push($('.j-original-price').text());
+        val.push($('#ad-attributes dd:first').text());
+        val.push(name);
+        val.push("","",url);
+        var output = val.join("\t");
+        console.log(output);
         chrome.runtime.sendMessage({"message": "open_new_tab", "data": output});
         //alert(stock_id + " " + ad_name + " " + ad_id + " " + price);
     }
